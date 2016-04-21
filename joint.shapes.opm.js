a6b4fa50e38cd118db5b0a22a20dfa261482a7f2
avivsugarman <avivsugarman@gmail.com>
2016-04-21 13:55:26 +0300

    Created ISO standard arrowheads and also expanded OPM library with ISO arrowheads and Lollipop.

--- joint.shapes.opm.js
+++ joint.shapes.opm.js
@@ -15,3 +15,26 @@ joint.shapes.opm.StateFinal = joint.shapes.basic.Generic.extend({
             'text': { fill: 'black' ,'font-family':'Arial' ,'font-size': 14, 'ref-x': .5, 'ref-y': .5, ref: 'rect', 'y-alignment': 'middle', 'x-alignment': 'middle' } }
     }, joint.shapes.basic.Generic.prototype.defaults)
 });
+
+// joint.shapes.opm.Link = joint.dia.Link.extend({
+//     renderMarkup('polygon')
+//     defaults:
+//         type: 'opm.Link',
+//         attrs: { '.marker-target': { 'points': "-2,0 -5,5 5,0 -5,-5", 'fill': "red", 'stroke': "black", 'stroke-width': 2 } , '.connection':{'stroke-width': 2} },
+//         smooth: false
+// });
+
+joint.shapes.opm.Link = joint.dia.Link.extend({
+    defaults: {
+        type: 'opm.Link',
+        attrs: { fill: '#f2f2f2', '.marker-target': { d: 'M 10,35 L -15,25 L 10,15 L0,25 L 10,35 M 0,25 L 90,25', fill: 'none', 'stroke-width': 2 }, '.connection':{'stroke-width': 2} }
+    }
+});
+
+joint.shapes.opm.Lollipop = new joint.dia.Link.extend({
+    defaults: {
+        type: 'opm.Link',
+        attrs : {'.marker-target': { fill: '#f2f2f2' ,d: 'M 10 10 m -5 0 a 5 5 0 1 0 10 0 a 5 5 0 1 0 -10 0','stroke-width': 2},'.connection':{'stroke-width': 2}}
+    }
+});
+
