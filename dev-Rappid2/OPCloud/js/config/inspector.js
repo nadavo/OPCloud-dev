741711382641285c1033fc7b40413f3f686b4e6d
nadavo <nadavo@gmail.com>
2016-11-30 02:10:47 +0200

    Fix Agent Link Color and Add Source Link Type
    + cleaned up opm.link inspector code

--- dev-Rappid2/OPCloud/js/config/inspector.js
+++ dev-Rappid2/OPCloud/js/config/inspector.js
@@ -109,10 +109,15 @@ App.config = App.config || {};
             { value: 'M 0 0 a 5 5 0 1 0 10 0 a 5 5 0 1 0 -10 0 M 10,0 L 25,0', content: 'Agent Link' }
         ],
 
-        linkType: [
+        SourceLinkType: [
+            { value: { d:'' }, content: 'None' },
+            { value: { fill: '#f2f2f2' ,d: 'M 8,33 L -12,25 L 8,17 L0,25 L 8,33 M 0,25 L 10,25','stroke-width': 2}, content: 'Consumption Link' }
+        ],
+
+        DestLinkType: [
             { value: { fill: '#f2f2f2' ,d: 'M 8,33 L -12,25 L 8,17 L0,25 L 8,33 M 0,25 L 10,25','stroke-width': 2}, content: 'Consumption Link' },
             { value: { fill: '#f2f2f2' ,d: 'M 0 0 a 5 5 0 1 0 10 0 a 5 5 0 1 0 -10 0 M 10,0 L 25,0','stroke-width': 2}, content: 'Instrument Link' },
-            { value: { fill: '#000000', d:'M 0 0 a 5 5 0 1 0 10 0 a 5 5 0 1 0 -10 0 M 10,0 L 25,0' ,'stroke-width': 2}, content: 'Agent Link' }
+            { value: { fill: '#000000' ,d:'M 0 0 a 5 5 0 1 0 10 0 a 5 5 0 1 0 -10 0 M 10,0 L 25,0','stroke-width': 2}, content: 'Agent Link' }
         ],
 
         strokeWidth: [
@@ -146,172 +151,21 @@ App.config = App.config || {};
         'opm.Link': {
             inputs: {
                 attrs: {
-                    '.connection': {
-                        'stroke-width': {
-                            type: 'range',
-                            min: 0,
-                            max: 50,
-                            defaultValue: 1,
-                            unit: 'px',
-                            group: 'connection',
-                            label: 'stroke width',
-                            index: 1
-                        },
-                        'stroke': {type: 'color', group: 'connection', label: 'stroke color', index: 2},
-                        'stroke-dasharray': {
-                            type: 'select',
-                            options: ['0', '1', '5,5', '5,10', '10,5', '5,1', '15,10,5,10,15'],
-                            group: 'connection',
-                            label: 'stroke dasharray',
-                            index: 3
-                        }
-                    },
                     '.marker-source': {
-                        transform: {
-                            type: 'range',
-                            min: 1,
-                            max: 15,
-                            unit: 'x scale',
-                            defaultValue: 'scale(1)',
-                            valueRegExp: '(scale\\()(.*)(\\))',
-                            group: 'marker-source',
-                            label: 'source arrowhead size',
-                            index: 1
-                        },
-                        fill: {type: 'color', group: 'marker-source', label: 'source arrowhead color', index: 2}
-                    },
-                    /*'marker-source': {
-                     type: 'select-box',
-                     options: options.linkType,
-                     group: 'marker-source',
-                     label: 'Source Link Type',
-                     index: 1
-                     },*/
-                    /*'.marker-source': {
-                     d: {
-                     type: 'select-box',
-                     options: options.arrowheadType,
-                     group: 'marker-source',
-                     label: 'Source Link Type',
-                     index: 1
-                     },
-                     fill: {
-                     type: 'color-palette',
-                     options: options.colorPalette,
-                     group: 'marker-source',
-                     label: 'Color',
-                     when: { ne: { 'attrs/.marker-source/transform': 'scale(0.001)'}},
-                     index: 2
-                     }
-                     },*/
-                    '.marker-target': {
-                        'd': {
                             type: 'select-box',
-                            options: options.arrowheadType,
+                            options: options.SourceLinkType,
+                            defaultValue: {d:''},
+                            group: 'marker-source',
+                            label: 'Link Type',
+                            index: 1,
+                    },
+                    '.marker-target': {
+                            type: 'select-box',
+                            options: options.DestLinkType,
                             group: 'marker-target',
                             label: 'Link Type',
                             index: 1,
-                        }
-                        /*fill: {
-                         type: 'color-palette',
-                         options: options.colorPalette,
-                         group: 'marker-target',
-                         label: 'Color',
-                         when: { ne: { 'attrs/.marker-target/transform': 'scale(0.001)'}},
-                         index: 2
-                         },*/
-                        /*transform: {
-                         type: 'range',
-                         min: 1,
-                         max: 15,
-                         unit: 'x scale',
-                         defaultValue: 'scale(1)',
-                         valueRegExp: '(scale\\()(.*)(\\))',
-                         group: 'marker-target',
-                         label: 'target arrowhead size',
-                         index: 3
-                         }*/
-                        /*d: {
-                         type: 'select-box',
-                         options: options.arrowheadType,
-                         group: 'marker-target',
-                         label: 'Target Link Type',
-                         index: 1
-                         },
-                         fill: {
-                         type: 'color-palette',
-                         options: options.colorPalette,
-                         group: 'marker-target',
-                         label: 'Color',
-                         when: { ne: { 'attrs/.marker-target/transform': 'scale(0.001)'}},
-                         index: 2
-                         }
-                         }*/
                     },
-                    /*smooth: {type: 'toggle', group: 'connection', index: 4},
-                     manhattan: {type: 'toggle', group: 'connection', index: 5},
-                     labels: {
-                     type: 'list',
-                     group: 'labels',
-                     attrs: {
-                     label: {'data-tooltip': 'Set (possibly multiple) labels for the link'},
-                     item: {
-                     type: 'object',
-                     properties: {
-                     position: {
-                     type: 'range',
-                     min: 0.1,
-                     max: .9,
-                     step: .1,
-                     defaultValue: .5,
-                     label: 'position',
-                     index: 2,
-                     attrs: {label: {'data-tooltip': 'Position the label relative to the source of the link'}}
-                     },
-                     attrs: {
-                     text: {
-                     text: {
-                     type: 'text',
-                     label: 'text',
-                     defaultValue: 'label',
-                     index: 1,
-                     attrs: {label: {'data-tooltip': 'Set text of the label'}}
-                     }
-                     }
-                     }
-                     }
-                     }
-                     }
-                     },*/
-                    /* router: {
-                     name: {
-                     type: 'select-button-group',
-                     options: options.router,
-                     group: 'connection',
-                     label: 'Connection type',
-                     index: 1
-                     },
-                     args: {
-                     side: {
-                     type: 'select-box',
-                     options: options.side,
-                     placeholder: 'Pick a side',
-                     group: 'connection',
-                     label: 'Anchors side',
-                     when: { eq: { 'router/name': 'oneSide' }, otherwise: { unset: true }},
-                     index: 2
-                     }
-                     }
-                     },*/
-                    /*connector: {
-                     name: {
-                     type: 'select-button-group',
-                     options: options.connector,
-                     group: 'connection',
-                     label: 'Connection style',
-                     index: 3
-                     }
-                     },*/
                     labels: {
                         type: 'list',
                         group: 'labels',
@@ -345,7 +199,7 @@ App.config = App.config || {};
                                 },
                                 position: {
                                     type: 'select-box',
-                                    options: options.labelPosition || [],
+                                    options: options.labelPosition,
                                     defaultValue: 0.5,
                                     label: 'Position',
                                     index: 2,
@@ -360,26 +214,26 @@ App.config = App.config || {};
                             }
                         }
                     }
-                    }
-                },
-                groups: {
-                    connection: {
-                        label: 'Connection',
-                        index: 1
-                    },
-                    'marker-source': {
-                        label: 'Source marker',
-                        index: 2
-                    },
-                    'marker-target': {
-                        label: 'Target marker',
-                        index: 3
-                    },
-                    labels: {
-                        label: 'Labels',
-                        index: 4
-                    }
                 }
+            },
+            groups: {
+                /*connection: {
+                    label: 'Connection',
+                    index: 1
+                },*/
+                'marker-source': {
+                    label: 'Source marker',
+                    index: 1
+                },
+                'marker-target': {
+                    label: 'Target marker',
+                    index: 2
+                },
+                labels: {
+                    label: 'Labels',
+                    index: 3
+                }
+            }
         },
 
         'opm.Object': {
@@ -3083,3 +2937,163 @@ App.config = App.config || {};
     };
 
 })();
+
+
+/*'.connection': {
+ 'stroke-width': {
+ type: 'range',
+ min: 0,
+ max: 50,
+ defaultValue: 1,
+ unit: 'px',
+ group: 'connection',
+ label: 'stroke width',
+ index: 1
+ },
+ 'stroke': {type: 'color', group: 'connection', label: 'stroke color', index: 2},
+ 'stroke-dasharray': {
+ type: 'select',
+ options: ['0', '1', '5,5', '5,10', '10,5', '5,1', '15,10,5,10,15'],
+ group: 'connection',
+ label: 'stroke dasharray',
+ index: 3
+ }
+ },*/
+/*'.marker-source': {
+ transform: {
+ type: 'range',
+ min: 1,
+ max: 15,
+ unit: 'x scale',
+ defaultValue: 'scale(1)',
+ valueRegExp: '(scale\\()(.*)(\\))',
+ group: 'marker-source',
+ label: 'source arrowhead size',
+ index: 1
+ },
+ fill: {type: 'color', group: 'marker-source', label: 'source arrowhead color', index: 2}
+ },*/
+/*'marker-source': {
+ type: 'select-box',
+ options: options.linkType,
+ group: 'marker-source',
+ label: 'Source Link Type',
+ index: 1
+ },*/
+/*'.marker-source': {
+ d: {
+ type: 'select-box',
+ options: options.arrowheadType,
+ group: 'marker-source',
+ label: 'Source Link Type',
+ index: 1
+ },
+ fill: {
+ type: 'color-palette',
+ options: options.colorPalette,
+ group: 'marker-source',
+ label: 'Color',
+ when: { ne: { 'attrs/.marker-source/transform': 'scale(0.001)'}},
+ index: 2
+ }
+ },*/
+/*fill: {
+ type: 'color-palette',
+ options: options.colorPalette,
+ group: 'marker-target',
+ label: 'Color',
+ when: { ne: { 'attrs/.marker-target/transform': 'scale(0.001)'}},
+ index: 2
+ },*/
+/*transform: {
+ type: 'range',
+ min: 1,
+ max: 15,
+ unit: 'x scale',
+ defaultValue: 'scale(1)',
+ valueRegExp: '(scale\\()(.*)(\\))',
+ group: 'marker-target',
+ label: 'target arrowhead size',
+ index: 3
+ }*/
+/*d: {
+ type: 'select-box',
+ options: options.arrowheadType,
+ group: 'marker-target',
+ label: 'Target Link Type',
+ index: 1
+ },
+ fill: {
+ type: 'color-palette',
+ options: options.colorPalette,
+ group: 'marker-target',
+ label: 'Color',
+ when: { ne: { 'attrs/.marker-target/transform': 'scale(0.001)'}},
+ index: 2
+ }
+ }*/
+/*},*/
+/*smooth: {type: 'toggle', group: 'connection', index: 4},
+ manhattan: {type: 'toggle', group: 'connection', index: 5},
+ labels: {
+ type: 'list',
+ group: 'labels',
+ attrs: {
+ label: {'data-tooltip': 'Set (possibly multiple) labels for the link'},
+ item: {
+ type: 'object',
+ properties: {
+ position: {
+ type: 'range',
+ min: 0.1,
+ max: .9,
+ step: .1,
+ defaultValue: .5,
+ label: 'position',
+ index: 2,
+ attrs: {label: {'data-tooltip': 'Position the label relative to the source of the link'}}
+ },
+ attrs: {
+ text: {
+ text: {
+ type: 'text',
+ label: 'text',
+ defaultValue: 'label',
+ index: 1,
+ attrs: {label: {'data-tooltip': 'Set text of the label'}}
+ }
+ }
+ }
+ }
+ }
+ }
+ },*/
+/* router: {
+ name: {
+ type: 'select-button-group',
+ options: options.router,
+ group: 'connection',
+ label: 'Connection type',
+ index: 1
+ },
+ args: {
+ side: {
+ type: 'select-box',
+ options: options.side,
+ placeholder: 'Pick a side',
+ group: 'connection',
+ label: 'Anchors side',
+ when: { eq: { 'router/name': 'oneSide' }, otherwise: { unset: true }},
+ index: 2
+ }
+ }
+ },*/
+/*connector: {
+ name: {
+ type: 'select-button-group',
+ options: options.connector,
+ group: 'connection',
+ label: 'Connection style',
+ index: 3
+ }
+ },*/
